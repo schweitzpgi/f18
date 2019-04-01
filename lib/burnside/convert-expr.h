d0ae4838997c29a836efc7711f7303192280a8fd
Eric Schweitz <eschweitz@nvidia.com>
2019-04-01 11:09:03 -0700

    Burnside bridge implementation
    adding types - expand sequence type to support Fortran descriptor information
    add code to lower function type signatures to LLVM types
    add support and lowering for an if-then-else construct
    add support and lowering for SELECT X operations
    add address arithmetic to canonicalize
    changes to start adding new concepts to FIR
    start adding top-level special forms
    convert the build system to work as an in-tree build
    finish draft of pretty parsers and printers
    finish the pretty printer/parser
    add "How To Lower" doc
    changes to put optional attributes in and position them before the type
    allow unquoted identifiers
    turn off warnings on Mac
    implement recursive record type (round trips)
    Add a small framework to lower intrinsic calls to runtime calls.
    refine type checking, add some sketches of tests, etc.
    convert MemToReg to template form
    add converter stubs for all FIR ops
    move the bridge from FIR to standard to be a transformation pass
    work on tilikum bridge, lowering types
    add complex primitives; start lowering some FIR to LLVM IR
    add code to allow cleanup on attribute dicts
    add FirEndOp converter
    add code to lower addc and subc ops
    add lowering for mulc and divc
    add code to support FP128 and X86_FP80
    add real primitives to FIR to support extended precision types
    add float primitives for !fir.real types that are not supported in std dialect
    add option for user control of KIND mappings
    Refactor intrinsic function lowering facility
    add more interface to allow source positions to be tracked for functions
    identified structs; box ops; add complex changes from 8c33745
    fix type passed to InsertValueOp and ExtractValueOp builder
    sync to latest MLIR and get rid of the `fir.icall` operation
    Change the readme to explain how to build FIR.
    Update FIR language document.
    fix bug with overly exuberate merging
    create Transforms folder
    clone the MLIR CSE pass for modification
    implementation of basic CSE

---
+++ lib/burnside/convert-expr.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FORTRAN_BURNSIDE_CONVERT_EXPR_H_
+#define FORTRAN_BURNSIDE_CONVERT_EXPR_H_
+
+#include "intrinsics.h"
+
+/// [Coding style](https://llvm.org/docs/CodingStandards.html)
+
+namespace mlir {
+class Location;
+class OpBuilder;
+class Type;
+class Value;
+}  // mlir
+
+namespace fir {
+class AllocaExpr;
+}  // fir
+
+namespace Fortran {
+namespace common {
+class IntrinsicTypeDefaultKinds;
+}  //  common
+namespace evaluate {
+template<typename> class Expr;
+class SomeType;
+}  // evaluate
+namespace semantics {
+class Symbol;
+}  // semantics
+
+namespace burnside {
+
+class SymMap;
+
+mlir::Value *createSomeExpression(mlir::Location loc, mlir::OpBuilder &builder,
+    evaluate::Expr<evaluate::SomeType> const &expr, SymMap &symMap,
+    common::IntrinsicTypeDefaultKinds const &defaults,
+    IntrinsicLibrary const &intrinsics);
+mlir::Value *createSomeAddress(mlir::Location loc, mlir::OpBuilder &builder,
+    evaluate::Expr<evaluate::SomeType> const &expr, SymMap &symMap,
+    common::IntrinsicTypeDefaultKinds const &defaults,
+    IntrinsicLibrary const &intrinsics);
+
+mlir::Value *createTemporary(mlir::Location loc, mlir::OpBuilder &builder,
+    SymMap &symMap, mlir::Type type, const semantics::Symbol *symbol);
+
+}  // burnside
+}  // Fortran
+
+#endif  // FORTRAN_BURNSIDE_CONVERT_EXPR_H_
