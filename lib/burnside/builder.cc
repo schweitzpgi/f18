67283ebbda8e22815528cf91e696cfbf5d80a1b9
Eric Schweitz <eschweitz@nvidia.com>
2020-01-02 13:04:57 -0800

    port over to MLIR post the merge of MLIR to the monorepo

--- lib/burnside/builder.cc
+++ lib/burnside/builder.cc
@@ -1,16 +1,10 @@
-// Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+//===-- lib/burnside/builder.cc ---------------------------------*- C++ -*-===//
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//===----------------------------------------------------------------------===//
 
 #include "builder.h"
 #include "bridge.h"
@@ -44,20 +38,20 @@ M::FuncOp B::getNamedFunction(M::ModuleOp module, llvm::StringRef name) {
   return module.lookupSymbol<M::FuncOp>(name);
 }
 
-void B::SymMap::addSymbol(Se::SymbolRef symbol, M::Value *value) {
+void B::SymMap::addSymbol(Se::SymbolRef symbol, M::Value value) {
   symbolMap.try_emplace(&*symbol, value);
 }
 
-M::Value *B::SymMap::lookupSymbol(Se::SymbolRef symbol) {
+M::Value B::SymMap::lookupSymbol(Se::SymbolRef symbol) {
   auto iter{symbolMap.find(&*symbol)};
   return (iter == symbolMap.end()) ? nullptr : iter->second;
 }
 
-void B::SymMap::pushShadowSymbol(Se::SymbolRef symbol, M::Value *value) {
+void B::SymMap::pushShadowSymbol(Se::SymbolRef symbol, M::Value value) {
   // find any existing mapping for symbol
   auto iter{symbolMap.find(&*symbol)};
   const Se::Symbol *sym{nullptr};
-  M::Value *val{nullptr};
+  M::Value val;
   // if mapping exists, save it on the shadow stack
   if (iter != symbolMap.end()) {
     sym = iter->first;
