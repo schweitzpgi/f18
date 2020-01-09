84c3ad43b5da715cb5728542cc5cb6e97e94dfc5
Gary Klimowicz <gklimowicz@pobox.com>
2020-01-09 12:06:11 -0800

    Respond to review comments
    Fix trailing comment lines (===---...---=== not just dashes).
    Fix header in test file geenrator test/burnside/expr-test-generator.cc.
    Leave existing headers in files taken from MLIR.
    
    Conflicts:
    	include/fir/FIROps.h
    	include/optimizer/FIROps.td
    	lib/burnside/complex-handler.h
    	lib/burnside/convert-expr.h
    	lib/burnside/convert-type.h
    	lib/burnside/mixin.h
    	lib/optimizer/CodeGen.cpp
    	lib/optimizer/StdConverter.cpp
    	lib/optimizer/Transforms/CSE.cpp
    	tools/bbc/bbc.cpp

--- include/optimizer/FIRType.h
+++ include/optimizer/FIRType.h
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//----------------------------------------------------------------------------//
+//===----------------------------------------------------------------------===//
 
 #ifndef OPTIMIZER_FIRTYPE_H
 #define OPTIMIZER_FIRTYPE_H
