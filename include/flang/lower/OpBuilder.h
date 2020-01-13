08b5eb37889299dead2325fb55d1a30eaca46f75
Jean Perier <jperier@nvidia.com>
2020-01-13 03:35:16 -0800

    Refactor character assignment lowering Facilitate OpBuilder manipulations by introducing the OpBuilderWrapper class that automatically add the location and help removing the boilerplate to create intreger constants. Create a LoopCreator class that inherits this class and helps generating simple (one nesting level) loops. Create a ChararacterOpsCreator that also inherits OpBuilderWrapper and provides character copy/padding implementation.

--- include/flang/lower/OpBuilder.h
+++ include/flang/lower/OpBuilder.h
@@ -20,6 +20,11 @@ namespace llvm {
 class StringRef;
 }
 
+namespace fir {
+class CharacterType;
+class ReferenceType;
+} // namespace fir
+
 namespace Fortran {
 namespace parser {
 class CookedSource;
@@ -53,6 +58,71 @@ public:
     symbolMap.clear();
     shadowStack.clear();
   }
+};
+
+/// Helper class that can be inherited from in order to
+/// facilitate mlir::OpBuilder usage.
+class OpBuilderHandler {
+public:
+  OpBuilderHandler(mlir::OpBuilder &b, mlir::Location l) : builder{b}, loc{l} {}
+  template <typename T, typename... Args>
+  auto create(Args... args) {
+    return builder.create<T>(loc, std::forward<Args>(args)...);
+  }
+  mlir::Value getIntegerConstant(mlir::Type integerType, std::int64_t);
+
+protected:
+  mlir::OpBuilder &builder;
+  mlir::Location loc;
+};
+
+/// Facilitate lowering to fir::loop
+class LoopCreator : public OpBuilderHandler {
+public:
+  LoopCreator(mlir::OpBuilder &b, mlir::Location l) : OpBuilderHandler{b, l} {}
+  LoopCreator(OpBuilderHandler &b) : OpBuilderHandler{b} {}
+  // In genLoop functions, lb, ub, and count arguments must have integer types.
+  // They will be automatically converted the IndexType if needed.
+  // Build loop [lb, ub) with step "step".
+  using BodyGenerator = std::function<void(OpBuilderHandler &, mlir::Value)>;
+  void genLoop(mlir::Value lb, mlir::Value ub, mlir::Value step,
+               const BodyGenerator &bodyGenerator);
+  /// Build loop [lb, ub) with step 1.
+  void genLoop(mlir::Value lb, mlir::Value ub,
+               const BodyGenerator &bodyGenerator);
+  /// Build loop [0, count) with step 1.
+  void genLoop(mlir::Value count, const BodyGenerator &bodyGenerator);
+
+private:
+  mlir::Type getIndexType();
+  mlir::Value convertToIndexType(mlir::Value integer);
+};
+
+/// Facilitate lowering of CHARACTER operation
+class CharacterOpsCreator : public OpBuilderHandler {
+public:
+  CharacterOpsCreator(mlir::OpBuilder &b, mlir::Location l)
+      : OpBuilderHandler{b, l} {}
+  CharacterOpsCreator(OpBuilderHandler &b) : OpBuilderHandler{b} {}
+  /// Interchange format to avoid inserting unbox/embox everywhere while
+  /// evaluating character expressions.
+  struct CharValue {
+    fir::ReferenceType getReferenceType();
+    fir::CharacterType getCharacterType();
+
+    mlir::Value reference;
+    mlir::Value len;
+  };
+
+  /// Copy count first characters of src into dest.
+  void genCopy(CharValue &dest, CharValue &src, mlir::Value count);
+  /// Pad str(from:to) with blanks. If to <= from, no padding is done.
+  void genPadding(CharValue &str, mlir::Value from, mlir::Value to);
+  /// allocate (on the stack) storage for character given the kind and length.
+  CharValue createTemp(fir::CharacterType type, mlir::Value len);
+
+private:
+  mlir::Value getBlankConstant(fir::CharacterType type);
 };
 
 /// Get the current Module
