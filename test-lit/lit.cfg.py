dbb267f4f12e9273e67aec8076834111428c8a11
Jean Perier <jperier@nvidia.com>
2020-01-16 06:26:36 -0800

    Make flang lit test run with in-tree builds - Fix flang path to export to lit config - Add ".fir" extension in lit config to be considered as tests. Tested with intree builds, it will probably still not work with out-of-tree builds due to llvm-lit paths issues.

--- test-lit/lit.cfg.py
+++ test-lit/lit.cfg.py
@@ -28,7 +28,7 @@ config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 # suffixes: A list of file extensions to treat as test files.
 config.suffixes = ['.f', '.F', '.ff','.FOR', '.for', '.f77', '.f90', '.F90',
                    '.ff90', '.f95', '.F95', '.ff95', '.fpp', '.FPP', '.cuf',
-                   '.CUF', '.f18', '.F18']
+                   '.CUF', '.f18', '.F18', '.fir' ]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
