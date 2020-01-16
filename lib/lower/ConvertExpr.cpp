cbcf20a6cfda092592bf027d5ba5c1723d9af37e
Jean Perier <jperier@nvidia.com>
2020-01-16 04:00:03 -0800

    Move ComplexHandler to OpBuilder.h and refactor it a bit Its interface remains unchanged, but it now inherits the recently added OpBuilderHandler class to make some op creation easier. Also remove the implementation from the header into OpBuilder.cpp so that the include file is cleaner. ComplexHandler.h is deleted.
    Use LoopBuilder helper to lower do loops.
    Also rename some classes and functions accodring to review comments
    here: https://github.com/schweitzpgi/f18/pull/38
    `xxCreator` -> `xxBuilder`

--- lib/lower/ConvertExpr.cpp
+++ lib/lower/ConvertExpr.cpp
@@ -18,7 +18,6 @@
 #include "fir/Dialect/FIROps.h"
 #include "fir/Dialect/FIRType.h"
 #include "flang/lower/Bridge.h"
-#include "flang/lower/ComplexHandler.h"
 #include "flang/lower/ConvertType.h"
 #include "flang/lower/OpBuilder.h"
 #include "flang/lower/Runtime.h"
@@ -280,7 +279,7 @@ class ExprLowering {
 
   template <int KIND>
   M::Value genval(Ev::ComplexComponent<KIND> const &part) {
-    return ComplexHandler{builder, getLoc()}.extractComplexPart(
+    return ComplexOpsBuilder{builder, getLoc()}.extractComplexPart(
         genval(part.left()), part.isImaginaryPart);
   }
 
@@ -362,7 +361,7 @@ class ExprLowering {
 
   template <int KIND>
   M::Value genval(Ev::ComplexConstructor<KIND> const &op) {
-    return ComplexHandler{builder, getLoc()}.createComplex(
+    return ComplexOpsBuilder{builder, getLoc()}.createComplex(
         KIND, genval(op.left()), genval(op.right()));
   }
   template <int KIND>
@@ -396,7 +395,7 @@ class ExprLowering {
       bool eq{op.opr == Co::RelationalOperator::EQ};
       assert(eq || op.opr == Co::RelationalOperator::NE &&
                        "relation undefined for complex");
-      result = ComplexHandler{builder, getLoc()}.createComplexCompare(
+      result = ComplexOpsBuilder{builder, getLoc()}.createComplexCompare(
           genval(op.left()), genval(op.right()), eq);
     } else {
       static_assert(TC == CharacterCat);
