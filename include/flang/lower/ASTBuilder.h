2c026b6c69bcc5fb729202a426eb55a982afdd3a
Eric Schweitz <eschweitz@nvidia.com>
2020-01-28 14:59:42 -0800

    review comments

--- include/flang/lower/ASTBuilder.h
+++ include/flang/lower/ASTBuilder.h
@@ -1,4 +1,4 @@
-//===-- lib/lower/ast-builder.h ---------------------------------*- C++ -*-===//
+//===-- include/flang/lower/AstBuilder.h ------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
