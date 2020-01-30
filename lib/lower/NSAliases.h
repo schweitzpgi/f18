c2d229734392aac4a7fdb247f1f97db6e570a23d
Eric Schweitz <eschweitz@nvidia.com>
2020-01-30 12:01:00 -0800

    add aliases

---
+++ lib/lower/NSAliases.h
@@ -0,0 +1,21 @@
+//===-- NSAliases.h ---------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FORTRAN_LOWER_NSALIASES_H
+#define FORTRAN_LOWER_NSALIASES_H
+
+namespace Br = Fortran::lower;
+namespace Co = Fortran::common;
+namespace Ev = Fortran::evaluate;
+namespace L = llvm;
+namespace Ma = Fortran::lower::mangle;
+namespace M = mlir;
+namespace Pa = Fortran::parser;
+namespace Se = Fortran::semantics;
+
+#endif // FORTRAN_LOWER_NSALIASES_H
