668bcf8811365111e1b5a740666171fb15abfb1c
Eric <eschweitz@nvidia.com>
2020-02-04 16:00:10 -0800

    merge fir directory with opt
    Conflicts:
    	lib/CMakeLists.txt

--- include/flang/optimizer/Transforms/Passes.h
+++ include/flang/optimizer/Transforms/Passes.h
@@ -1,4 +1,4 @@
-//===-- include/fir/Transforms/Passes.h -------------------------*- C++ -*-===//
+//===-- optimizer/Transforms/Passes.h ---------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
