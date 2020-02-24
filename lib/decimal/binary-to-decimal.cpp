13d700f2556700924abf0420f99a2a2a04cdc14b
Eric <eschweitz@nvidia.com>
2020-02-24 13:13:39 -0800

    add back the missing template instantiations to avoid link-time failures

--- lib/decimal/binary-to-decimal.cpp
+++ lib/decimal/binary-to-decimal.cpp
@@ -316,6 +316,13 @@ void BigRadixFloatingPointNumber<PREC,
   }
 }
 
+template void BigRadixFloatingPointNumber<8,16>::LoseLeastSignificantDigit();
+template void BigRadixFloatingPointNumber<11,16>::LoseLeastSignificantDigit();
+template void BigRadixFloatingPointNumber<24,16>::LoseLeastSignificantDigit();
+template void BigRadixFloatingPointNumber<53,16>::LoseLeastSignificantDigit();
+template void BigRadixFloatingPointNumber<64,16>::LoseLeastSignificantDigit();
+template void BigRadixFloatingPointNumber<112,16>::LoseLeastSignificantDigit();
+
 template<int PREC>
 ConversionToDecimalResult ConvertToDecimal(char *buffer, std::size_t size,
     enum DecimalConversionFlags flags, int digits,
