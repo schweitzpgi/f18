ac10a8c35233136fca9104a70173d734b6ea91a5
Eric <eric.schweitz@gmail.com>
2020-03-27 15:54:22 -0700

    more work on hello.f90 test

--- test/Examples/hello.f90
+++ test/Examples/hello.f90
@@ -1,6 +1,11 @@
+! Note: The Fortran runtime libraries have dependences on the C++ runtime and
+! LLVM runtime.  To work around the former, this test explicitly links in
+! libstdc++.a.  To work around the latter, the source of Common/enum-set.h was
+! hacked to exclude references to llvm ADTs.
+
 ! RUN: bbc %s -o - | tco | llc --filetype=obj -o %t.o
 ! RUN: cc -I%S/../.. %S/main.c -c -o %t.main.o
-! RUN: cc %t.o %t.main.o $(llvm-config --ldflags --libs) -L$(dirname $(which bbc))/../lib -lFortranRuntime -lFortranDecimal -lstdc++
+! RUN: cc %t.o %t.main.o -L%L -lFortranRuntime -lFortranDecimal -lstdc++
 ! RUN: ./a.out | FileCheck %s
 
 ! CHECK: Hello, World!
