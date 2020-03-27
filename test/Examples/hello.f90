ce62689e9cde2984bc8d734fb51a42da91b24290
Eric <eric.schweitz@gmail.com>
2020-03-27 15:55:28 -0700

    changes to make coordinate_of work for simple pointer arithmetic add test char01.fir other misc. changes

--- test/Examples/hello.f90
+++ test/Examples/hello.f90
@@ -1,11 +1,11 @@
 ! Note: The Fortran runtime libraries have dependences on the C++ runtime and
-! LLVM runtime.  To work around the former, this test explicitly links in
+! LLVM libraries.  To work around the former, this test explicitly links in
 ! libstdc++.a.  To work around the latter, the source of Common/enum-set.h was
 ! hacked to exclude references to llvm ADTs.
 
 ! RUN: bbc %s -o - | tco | llc --filetype=obj -o %t.o
 ! RUN: cc -I%S/../.. %S/main.c -c -o %t.main.o
-! RUN: cc %t.o %t.main.o -L%L -lFortranRuntime -lFortranDecimal -lstdc++
+! RUN: cc %t.o %t.main.o -L%L -lFortranRuntime -lFortranDecimal -lstdc++ -lm
 ! RUN: ./a.out | FileCheck %s
 
 ! CHECK: Hello, World!
