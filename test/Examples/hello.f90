9f5e5d197947a8678f9406f0e94614a7ef48877a
Eric Schweitz <eschweitz@nvidia.com>
2020-03-27 18:29:46 -0700

    add comment

--- test/Examples/hello.f90
+++ test/Examples/hello.f90
@@ -2,6 +2,7 @@
 ! LLVM libraries.  To work around the former, this test explicitly links in
 ! libstdc++.a.  To work around the latter, the source of Common/enum-set.h was
 ! hacked to exclude references to llvm ADTs.
+! Note: On linux, the Fortran runtime wants to include libm as well.
 
 ! RUN: bbc %s -o - | tco | llc --filetype=obj -o %t.o
 ! RUN: cc -I%S/../.. %S/main.c -c -o %t.main.o
