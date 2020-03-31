68eaa34ab7635be7b00e92d584731321392617f5
Eric <eschweitz@nvidia.com>
2020-03-31 15:17:48 -0700

    extend test

--- test/Lower/open.f90
+++ test/Lower/open.f90
@@ -7,4 +7,20 @@
 ! CHECK: call {{.*}}EndIoStatement
 
   open(8, file="foo", access="sequential")
+
+! CHECK: call {{.*}}BeginBackspace
+! CHECK: call {{.*}}EndIoStatement
+  backspace(8)
+  
+! CHECK: call {{.*}}BeginFlush
+! CHECK: call {{.*}}EndIoStatement
+  flush(8)
+  
+! CHECK: call {{.*}}BeginRewind
+! CHECK: call {{.*}}EndIoStatement
+  rewind(8)
+
+! CHECK: call {{.*}}BeginClose
+! CHECK: call {{.*}}EndIoStatement
+  close(8)
 end
