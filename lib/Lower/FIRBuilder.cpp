a3912076e7d00dbeeec2c466e51af4e446a5b5ee
Eric <eschweitz@nvidia.com>
2020-04-01 13:55:37 -0700

    use getModule add WhereOp folder

--- lib/Lower/FIRBuilder.cpp
+++ lib/Lower/FIRBuilder.cpp
@@ -74,8 +74,10 @@ mlir::Value Fortran::lower::FirOpBuilder::createTemporary(
   return ae;
 }
 
-fir::GlobalOp Fortran::lower::FirOpBuilder::createGlobal(mlir::Location loc, mlir::Type type, llvm::StringRef name, mlir::Attribute value, mlir::StringAttr linkage, bool isConst) {
-  auto module = getInsertionBlock()->getParentOp()->getParentOfType<mlir::ModuleOp>();
+fir::GlobalOp Fortran::lower::FirOpBuilder::createGlobal(
+    mlir::Location loc, mlir::Type type, llvm::StringRef name,
+    mlir::Attribute value, mlir::StringAttr linkage, bool isConst) {
+  auto module = getModule();
   auto insertPt = saveInsertionPoint();
   if (auto glob = module.lookupSymbol<fir::GlobalOp>(name))
     return glob;
